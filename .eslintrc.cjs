d4eab9235ffe0c2da52316460911920ccdba830a
ShovalShabi <shovalshabi@gmail.com>
2024-09-11 12:08:53 +0300

    Configured necessary parameters for the application

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare module "@env" {
+  export const EXPO_PORT: string;
+  export const EXPO_ENV: string;
+  export const EXPO_HOST_TYPE: string;
+  export const EXPO_JAVASCRIPT_MAPS_API_KEY: string;
+  export const EXPO_ORDER_BUS_PORT: string;
+}
