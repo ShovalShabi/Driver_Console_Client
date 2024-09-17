3c5011a7e486fd5010cd7ecae29c5c61abb21a5f
ShovalShabi <shovalshabi@gmail.com>
2024-09-17 09:57:19 +0300

    Adjusted App structure, also made some stub stations (need to remove later)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
+// App.tsx
 import React, { useState } from "react";
-import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import {
   Provider as PaperProvider,
   configureFonts,
   DefaultTheme,
 } from "react-native-paper";
 import Login from "./src/components/Login"; // Adjust path if needed
-import StationsList from "./src/components/StationsList"; // Adjust path if needed
-import Map from "./src/components/Map"; // Adjust path if needed
+import NavigationScreen from "./src/screens/NavigationScreen"; // Adjust path if needed
+import { IStation } from "./src/utils/IStation";
 
 const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -23,43 +23,26 @@ const App: React.FC = () => {
     }),
   };
 
+  // Define initial stations (can be fetched from a server upon login) -- Remove later!!!
+  const initialStations: IStation[] = [
+    { address: "Shoham St 3-5, Be'er Ya'akov", visited: false },
+    { address: "מרגולין/רמב''ם, Ness Ziona", visited: false },
+    { address: "Margolin/Komarov, Ness Ziona", visited: false },
+    {
+      address: "Kenyoter/Weizmann, Ness Ziona",
+      visited: false,
+    },
+  ];
+
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        <StatusBar barStyle="dark-content" />
-        <View style={styles.contentContainer}>
-          <View style={styles.stationsList}>
-            {isLoggedIn ? (
-              <StationsList stations={[]} />
-            ) : (
-              <Login onLoginSuccess={() => setIsLoggedIn(true)} />
-            )}
-          </View>
-          <View style={styles.map}>
-            <Map />
-          </View>
-        </View>
-      </SafeAreaView>
+      {isLoggedIn ? (
+        <NavigationScreen initialStations={initialStations} />
+      ) : (
+        <Login onLoginSuccess={() => setIsLoggedIn(true)} />
+      )}
     </PaperProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  contentContainer: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  stationsList: {
-    flex: 1,
-    marginRight: 8,
-  },
-  map: {
-    flex: 2,
-  },
-});
 
 export default App;
