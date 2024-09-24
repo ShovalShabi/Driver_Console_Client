dd89cf38c68bf0f05c81e8743cd40d018fe389c5
ShovalShabi <shovalshabi@gmail.com>
2024-09-24 21:32:25 +0300

    Added url for the backend websocker serivce

--- src/etc/loadVariables.ts
+++ src/etc/loadVariables.ts
@@ -4,6 +4,7 @@ import {
   EXPO_HOST_TYPE,
   EXPO_JAVASCRIPT_MAPS_API_KEY,
   EXPO_ORDER_BUS_PORT,
+  EXPO_WS_SERVER_ENDPOINT,
 } from "@env";
 
 // Define a function to retrieve environment variables
@@ -17,11 +18,14 @@ const getEnvVariables = () => {
   //TODO: Need to specify between two environments by EXPO_ENV
   const orderBusURL = `http://${EXPO_HOST_TYPE}:${EXPO_ORDER_BUS_PORT}`;
 
+  const webSocketOrderBusURL = `ws:///192.168.1.186:10093/${EXPO_WS_SERVER_ENDPOINT}`; //Temporary!!! later on there wil be the container name of the backend microservice orderbus
+
   return {
     port,
     env,
     apiGlobalKey,
     orderBusURL,
+    webSocketOrderBusURL,
   };
 };
 
