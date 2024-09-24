1e8ee3142436bf5a36c134326b062697aae60f07
ShovalShabi <shovalshabi@gmail.com>
2024-09-24 21:27:48 +0300

    Added option to the websocket messaging

--- env.d.ts
+++ env.d.ts
@@ -4,4 +4,5 @@ declare module "@env" {
   export const EXPO_HOST_TYPE: string;
   export const EXPO_JAVASCRIPT_MAPS_API_KEY: string;
   export const EXPO_ORDER_BUS_PORT: string;
+  export const EXPO_WS_SERVER_ENDPOINT: string;
 }
