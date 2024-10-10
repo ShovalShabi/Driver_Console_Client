4d0e1b7c0a332a03d77856666190af248f0c43dc
ShovalShabi <shovalshabi@gmail.com>
2024-10-10 10:23:21 +0300

    Changed the application structure

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
-// App.tsx
-import React, { useState } from "react";
+import React from "react";
 import {
-  Provider as PaperProvider,
   configureFonts,
   DefaultTheme,
+  Provider as PaperProvider,
 } from "react-native-paper";
-import Login from "./src/components/Login"; // Adjust path if needed
-import NavigationScreen from "./src/screens/NavigationScreen"; // Adjust path if needed
-import { IStation } from "./src/utils/IStation";
+import NavigationScreen from "./src/screens/NavigationScreen";
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const theme = {
     ...DefaultTheme,
     colors: {
@@ -23,24 +18,9 @@ const App: React.FC = () => {
     }),
   };
 
-  // Define initial stations (can be fetched from a server upon login) -- Remove later!!!
-  const initialStations: IStation[] = [
-    { address: "Shoham St 3-5, Be'er Ya'akov", visited: false },
-    { address: "מרגולין/רמב''ם, Ness Ziona", visited: false },
-    { address: "Margolin/Komarov, Ness Ziona", visited: false },
-    {
-      address: "Kenyoter/Weizmann, Ness Ziona",
-      visited: false,
-    },
-  ];
-
   return (
     <PaperProvider theme={theme}>
-      {isLoggedIn ? (
-        <NavigationScreen initialStations={initialStations} />
-      ) : (
-        <Login onLoginSuccess={() => setIsLoggedIn(true)} />
-      )}
+      <NavigationScreen />
     </PaperProvider>
   );
 };
