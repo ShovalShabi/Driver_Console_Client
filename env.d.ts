2b60e68840e32e26c4d0cfee68f577cf022a5ffd
ShovalShabi <shovalshabi@gmail.com>
2024-10-10 10:21:36 +0300

    Added environment variables for Auth and OrderBus Services, and defined them in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,12 @@
 declare module "@env" {
   export const EXPO_PORT: string;
   export const EXPO_ENV: string;
-  export const EXPO_HOST_TYPE: string;
   export const EXPO_JAVASCRIPT_MAPS_API_KEY: string;
   export const EXPO_ORDER_BUS_PORT: string;
-  export const EXPO_WS_SERVER_ENDPOINT: string;
+  export const EXPO_BCAKEND_SERVER_IP: string;
+  export const EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT: string;
+  export const EXPO_ORDER_BUS_SRVICE_DEV_PORT: string;
+  export const EXPO_ORDER_BUS_SRVICE_PROD_PORT: string;
+  export const EXPO_AUTH_SRVICE_DEV_PORT: string;
+  export const EXPO_AUTH_SRVICE_PROD_PORT: string;
 }
--- src/etc/loadVariables.ts
+++ src/etc/loadVariables.ts
@@ -1,31 +1,52 @@
 import {
   EXPO_PORT,
   EXPO_ENV,
-  EXPO_HOST_TYPE,
   EXPO_JAVASCRIPT_MAPS_API_KEY,
-  EXPO_ORDER_BUS_PORT,
-  EXPO_WS_SERVER_ENDPOINT,
+  EXPO_BCAKEND_SERVER_IP,
+  EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT,
+  EXPO_ORDER_BUS_SRVICE_DEV_PORT,
+  EXPO_ORDER_BUS_SRVICE_PROD_PORT,
+  EXPO_AUTH_SRVICE_DEV_PORT,
+  EXPO_AUTH_SRVICE_PROD_PORT,
 } from "@env";
 
 // Define a function to retrieve environment variables
 const getEnvVariables = () => {
   // Validate required variables or provide default values
-  const port = parseInt(EXPO_PORT || "6002", 10); // Default to 3001 if EXPO_PORT is not defined
+  let port = 0; // A default port for testing (OS assign the port)
   const env = EXPO_ENV || "dev"; // Default to 'dev' if EXPO_ENV is not defined
 
   const apiGlobalKey = EXPO_JAVASCRIPT_MAPS_API_KEY || "";
 
-  //TODO: Need to specify between two environments by EXPO_ENV
-  const orderBusURL = `http://${EXPO_HOST_TYPE}:${EXPO_ORDER_BUS_PORT}`;
+  let orderBusServiceURL, authServiceURL, webSocketOrderBusServiceURL;
 
-  const webSocketOrderBusURL = `ws:///192.168.1.186:10093/${EXPO_WS_SERVER_ENDPOINT}`; //Temporary!!! later on there wil be the container name of the backend microservice orderbus
+  if (env === "dev") {
+    port = parseInt(EXPO_PORT || "6002", 10); // Default to 6002 if EXPO_PORT is not defined
+    orderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_DEV_PORT}`;
+    authServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_AUTH_SRVICE_DEV_PORT}`;
+    webSocketOrderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_DEV_PORT}/${EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT}`;
+  } else if (env == "prod") {
+    port = parseInt(EXPO_PORT || "7002", 10); // Default to 6002 if EXPO_PORT is not defined
+    orderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_PROD_PORT}`;
+    authServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_AUTH_SRVICE_PROD_PORT}`;
+    webSocketOrderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_PROD_PORT}/${EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT}`;
+  } else {
+    // testing the application, using the services on dev mode
+    orderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_DEV_PORT}`;
+    authServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_AUTH_SRVICE_DEV_PORT}`;
+    webSocketOrderBusServiceURL = `http://${EXPO_BCAKEND_SERVER_IP}:${EXPO_ORDER_BUS_SRVICE_DEV_PORT}/${EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT}`;
+  }
 
+  console.log(
+    `Auth Service ${authServiceURL}\nOrderBus Service ${orderBusServiceURL}\nWeb Socket Endpoint ${webSocketOrderBusServiceURL}`
+  );
   return {
     port,
     env,
     apiGlobalKey,
-    orderBusURL,
-    webSocketOrderBusURL,
+    orderBusServiceURL,
+    authServiceURL,
+    webSocketOrderBusServiceURL,
   };
 };
 
