9a97391dbb797bf3d17584146a79e3c68ac788cf
ShovalShabi <shovalshabi@gmail.com>
2024-10-10 10:27:48 +0300

    Handled data encapsulation from backend response, might delete the other parameters if not needed

--- src/utils/IStation.ts
+++ src/utils/IStation.ts
@@ -1,7 +1,9 @@
 import { ILocation } from "./ILocation";
+import StationResponseTDO from "../dto/StationResponseDTO";
 
 export interface IStation {
   address: string;
   visited: boolean;
   coordinate?: ILocation | null;
+  data: StationResponseTDO | null;
 }
