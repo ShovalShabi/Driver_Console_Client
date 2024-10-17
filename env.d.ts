9bf32834410f2b980b5b670ee14b7d6c445a4a4d
ShovalShabi <shovalshabi@gmail.com>
2024-10-17 12:49:40 +0300

    Edited the environment variables as need for the applicatin to run

--- env.d.ts
+++ env.d.ts
@@ -2,11 +2,9 @@ declare module "@env" {
   export const EXPO_PORT: string;
   export const EXPO_ENV: string;
   export const EXPO_JAVASCRIPT_MAPS_API_KEY: string;
-  export const EXPO_ORDER_BUS_PORT: string;
   export const EXPO_BCAKEND_SERVER_IP: string;
+  export const EXPO_BCAKEND_SERVER_PORT: string;
+  export const EXPO_ORDER_BUS_SRVICE_ENDPOINT: string;
+  export const EXPO_AUTH_SRVICE_ENDPOINT: string;
   export const EXPO_ORDER_BUS_SRVICE_WS_ENDPOINT: string;
-  export const EXPO_ORDER_BUS_SRVICE_DEV_PORT: string;
-  export const EXPO_ORDER_BUS_SRVICE_PROD_PORT: string;
-  export const EXPO_AUTH_SRVICE_DEV_PORT: string;
-  export const EXPO_AUTH_SRVICE_PROD_PORT: string;
 }
