ca4cdb301474f2648905bf31085fa60f959da14c
ShovalShabi <shovalshabi@gmail.com>
2024-10-17 12:52:56 +0300

    Conigured reducer to save the entire ride route, should store much of memory

--- src/states/ridePlanningReducer.ts
+++ src/states/ridePlanningReducer.ts
@@ -1,27 +1,27 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
-import StationsRequestDTO from "../dto/StationsRequestDTO";
+import { IStation } from "../utils/IStation";
 
 // Action types (using createAction)
-export const planRide = createAction<StationsRequestDTO>("PLAN_RIDE");
+export const planRide = createAction<IStation[]>("PLAN_RIDE");
 export const resetRide = createAction("RESET_RIDE");
 
 // Initial state
 interface RidePlanningState {
-  stationsRequest: StationsRequestDTO | null;
+  stationsResponseArr: IStation[] | null;
 }
 
 const initialState: RidePlanningState = {
-  stationsRequest: null,
+  stationsResponseArr: [],
 };
 
 // Reducer
 const ridePlanningReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(planRide, (state, action) => {
-      state.stationsRequest = action.payload; // Set ride planning request
+      state.stationsResponseArr = action.payload; // Set ride planning response
     })
     .addCase(resetRide, (state) => {
-      state.stationsRequest = null; // Clear ride planning on reset
+      state.stationsResponseArr = null; // Clear ride planning on reset
     });
 });
 
