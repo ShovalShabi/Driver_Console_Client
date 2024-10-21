654ab870ffae78a5f885904459f22d5903c958dc
ShovalShabi <shovalshabi@gmail.com>
2024-10-22 01:19:15 +0300

    Altered the the Alert component to match the relevant scenarios

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,20 +1,42 @@
 import { Alert } from "react-native";
+import { ILocation } from "../utils/ILocation";
 
 // Function to show an alert when a ride request is received
 export const showRideRequestAlert = (
-  acceptRide: () => void,
-  cancelRide: () => void
+  payload: string, // Message payload from the backend
+  acceptRide: (targetStation: ILocation) => void,
+  cancelRide: (targetStation: ILocation) => void,
+  targetStation: ILocation
 ) => {
   Alert.alert(
     "Ride Request",
-    "You have a new ride request. Would you like to accept it?",
+    payload, // Use the backend's payload for the alert body
     [
       {
         text: "Cancel",
-        onPress: () => cancelRide(),
+        onPress: () => cancelRide(targetStation),
         style: "cancel",
       },
-      { text: "Accept", onPress: () => acceptRide() },
+      {
+        text: "Accept",
+        onPress: () => acceptRide(targetStation),
+      },
+    ],
+    { cancelable: false }
+  );
+};
+
+// Function to show an alert when a ride is canceled
+export const showRideCancellationAlert = (payload: string) => {
+  Alert.alert(
+    "Ride Canceled",
+    payload, // Display the payload from the backend for ride cancellation
+    [
+      {
+        text: "Confirm",
+        onPress: () => console.log("Ride cancellation confirmed"),
+        style: "default",
+      },
     ],
     { cancelable: false }
   );
